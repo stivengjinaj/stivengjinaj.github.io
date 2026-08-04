a10aa25f1fa2206342d1b9f58bfea25e9def7a01
stivengjinaj <stivengjinaj@hotmail.com>
2026-08-04 17:06:16 +0200

    fix: fixed inaccessible publications sections

--- src/AboutSection/AboutSection.tsx
+++ src/AboutSection/AboutSection.tsx
@@ -5,7 +5,7 @@ import Publications from "./Publications.tsx";
 
 function AboutSection({ref}: RefSection) {
     return (
-        <section id={"about"} ref={ref} className="min-h-screen pt-3 px-5 gap-2 flex flex-col items-center snap-start relative overflow-hidden">
+        <section id={"about"} ref={ref} className="min-h-screen pb-20 md:pb-0 pt-3 px-5 gap-2 flex flex-col items-center snap-start relative overflow-hidden">
             <AcademicCard/>
             <div className="grid grid-cols-1 xl:grid-cols-2 gap-2">
                 <ProfessionalExperiences />
